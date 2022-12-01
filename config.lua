6ed23c8c61bd9106ec699e5bda5b9c2941dcb383
DavidH <handuy099@gmail.com>
2022-12-01 15:41:59 +0700

    update

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 reload "user.plugins"
 reload "user.options"
-reload "user.treesitter"
 reload "user.keymaps"
+reload "user.treesitter"
 reload "user.lsp"
 reload "user.telescope"
 reload "user.dial"
