c00443a8c3c1e11eca230f7279b222dba2951e97
DavidH <handuy099@gmail.com>
2023-02-23 18:38:16 +0700

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# My LunarVim Config For Windows
+
+## Install
+```bash
+git clone git@github.com:hd929/lvim.git $HOME/.config/lvim
+git checkout Windows
+```
+
+```bash
+ln -s $HOME\.config\lvim\ $HOME\AppData\Local\lvim
+```
---
+++ config.lua
@@ -0,0 +1,14 @@
+reload "user.plugins"
+reload "user.options"
+reload "user.keymaps"
+reload "user.treesitter"
+reload "user.lsp"
+reload "user.telescope"
+reload "user.dial"
+reload "user.zen-mode"
+reload "user.smoothie"
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "tokyonight-night"
