c2f2dc73355b65c3568d726d0a4e4d1913fed307
DavidH <handuy099@gmail.com>
2023-05-09 17:40:33 +0700

    [LSP]: add python

---
+++ ftplugin/python.lua
@@ -0,0 +1,31 @@
+-- Advanced pyright configuration
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright", "jsonls" })
+
+local pyright_opts = {
+  single_file_support = true,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
+    },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- openFilesOnly, workspace
+        typeCheckingMode = "basic", -- off, basic, strict
+        useLibraryCodeForTypes = true
+      }
+    }
+  },
+}
+
+require("lvim.lsp.manager").setup("pyright", pyright_opts)
+
+-- setup formatting
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+-- lvim.format_on_save.pattern = { "*.py" }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", filetypes = { "python" } } }
