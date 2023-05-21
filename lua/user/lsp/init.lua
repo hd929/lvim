fa14b8a7bf6785d01bb3fe4be82d6c2b588cae75
DavidH <handuy099@gmail.com>
2023-05-21 18:34:44 +0700

    [LSP]: add emmet

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,17 +2,20 @@ vim.diagnostic.config({ virtual_text = false })
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "stylua",   filetypes = { "lua" } },
-  { command = "shfmt",    filetypes = { "sh", "zsh" } },
-  { command = "prettier", filetypes = { "css", "javascript" } },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "shfmt", filetypes = { "sh", "zsh" } },
+	{ command = "prettier", filetypes = { "css", "javascript", "scss" } },
+})
 
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 -- end
 
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "eslint_d", filetypes = { "javascript" } },
--- }
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "eslint_d", filetypes = { "javascript" } },
+})
+
+require("user.lsp.languages.js-ts")
+require("user.lsp.languages.emmet")
